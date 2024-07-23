f60e07a1283273c86b491adc89ca065364d47ac2
saraoliveir4 <sara.monteiro1234@gmail.com>
2024-07-23 13:32:25 -0300

    criação das classes model, controller, e interface repository

---
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,61 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo Nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo Nome deve conter no mínimo 5 e no máximo 100 caracteres!")
+	private String nomeProduto;
+
+	@NotNull(message = "O atributo Valor Unitário é obrigatório!")
+	private Float valorUnitario;
+
+	@NotNull(message = "O atributo Quantidade é obrigatório!")
+	private Integer quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public Float getValorUnitario() {
+		return valorUnitario;
+	}
+
+	public void setValorUnitario(Float valorUnitario) {
+		this.valorUnitario = valorUnitario;
+	}
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+}
